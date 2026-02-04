8a0edbff2a2ddd71ebfa44720a90a375df8f1c05
Nijarec <43285148+Nijarec@users.noreply.github.com>
2026-02-03 21:57:05 -0600

    Add script for Languages

---
+++ scriptLanguages.js
@@ -0,0 +1,129 @@
+const texts = {
+  es: {
+    title: "BERNARDO NIJAREC GÓMEZ RIZO",
+    subtitle: "Ingeniero Informático",
+    about: "Sobre mí",
+    aboutText: "Soy desarrollador de software con experiencia "
+    +"en desarrollo de aplicaciones administrativas y soluciones web."+
+    " Me interesa seguir aprendiendo y mejorar continuamente mis habilidades técnicas.",
+    projects: "Proyectos",
+    contact:"Contacto",
+    skills: "Habilidades",
+    experience:"Experiencia",
+    exp1_title: "Proyecto Modular Universidad de Guadalajara - GESTOR DE PROYECTO",
+    exp1_desc: `Realicé el proyecto final de mi carrera universitaria como ingeniero en informática, el
+cual llevó por nombre “GearWater”. Este proyecto consistió en el desarrollo de una
+aplicación para teléfonos móviles y otros dispositivos, enfocada en calcular la
+cantidad de agua necesaria para irrigar determinado cultivo, considerando el número
+de hectáreas y la ubicación geográfica proporcionada por el usuario. El cálculo se
+realizó mediante operaciones complejas que determinaban la cantidad de agua
+requerida, basándose en los datos ingresados por el usuario y en información obtenida
+por la propia aplicación, como el estado del clima, las horas de luz solar, la
+evaporación del agua, entre otros factores.
+El proyecto se completó de manera exitosa, cumpliendo con sus objetivos y
+obteniendo la aprobación del comité universitario, satisfaciendo plenamente sus
+expectativas. Todo esto lo hice usando <b>Java</b> dentro de <b>Android Studio</b>.`,
+    exp1_date: "ABRIL 2020 - DICIEMBRE 2020 | Compañia o institucion: Proyecto Final UDG",
+
+    exp2_title: "Desarrollo de sistema administrativo “Candy Spa” - GESTOR DE PROYECTO",
+    exp2_desc: ` Diseñé e implementé un software de gestión integral para un spa, con funcionalidades
+de punto de venta, control de inventarios, administración de clientes y empleados,
+generación de reportes y agenda de citas. El sistema optimizó los procesos internos,
+mejoró la atención al cliente y centralizó la información operativa en una única
+plataforma. Todo esto lo realice usando <b>Basic</b> en <b>Visual Studio</b> y <b>SQL Server</b>.`,
+    exp2_date: "MAYO 2016 | Compañia o institucion: Proyecto académico",
+
+    exp3_title: "Programa de cifrado de textos y archivos - GESTOR DE PROYECTO",
+    exp3_desc: ` Desarrollé una aplicación de escritorio llamada “ECS: Encrypter Control Security”
+capaz de encriptar y desencriptar textos utilizando una clave de acceso personalizada
+creada por el usuario. El sistema admite el cifrado de cualquier tipo de archivo,
+garantizando la seguridad y confidencialidad de la información. Incluye una interfaz
+amigable y validaciones para prevenir errores de clave o formato. Todo esto realizado
+usando <b>Basic</b> en <b>Visual Studio</b>.`,
+    exp3_date: "ENERO 2018 | Compañia o institucion: Proyecto académico",
+
+    exp4_title: "Sistema de gestión académica en línea - GESTOR DE PROYECTO",
+    exp4_desc: ` Desarrollé una plataforma web para instituciones educativas que permite a los
+profesores asignar tareas, subir materiales de apoyo y evaluar entregas, mientras que
+los estudiantes pueden enviar sus trabajos, consultar calificaciones y acceder a
+recursos académicos. El sistema centraliza la administración académica y mejora la
+comunicación entre docentes y alumnos, optimizando el seguimiento del progreso
+escolar. Todo esto lo realice usando <b>PHP</b> en <b>Atom</b> y <b>MySQL Workbench</b>.`,
+    exp4_date: "MARZO 2018 | Compañia o institucion: Proyecto académico",
+    education:"Educación",
+    edu1_degree:"Ingeniería Informática",
+    edu1_school:"Universidad de Guadalajara",
+    edu2_degree:"Técnico en Mantenimiento de Equipos y Sistemas",
+   project1_title:"Encriptador Cesar",
+   project1_desc:`Programa desarrollado en Visual Basic, que encripta textos a través de encriptación césar.`,
+   project2_title:"Reproductor de Musica",
+   project2_desc:`Es un reproductor de muscia crado con lenguaje Java en Android Studio`,
+   project3_title:"App Salarios Minimos en Mexico",
+   project3_desc:`Es una aplicación desarrollada con Java en Android Studio, pensada para mostrar los salarios minimos que deben de resivir los trabajadores segun su empleo.`
+  },
+  en: {
+    title: "BERNARDO NIJAREC GÓMEZ RIZO",
+    subtitle: "Software Engineer",
+    about: "About Me",
+    aboutText: "I am a software developer with experience"+
+    "in developing administrative applications and web solutions."+
+     "I am interested in continuing to learn and continuously improving my technical skills.",
+    projects: "Projects",
+    contact:"Contact",
+    skills: "Skills",
+    experience:"Experience",
+    exp1_title: "Modular University Project – University of Guadalajara | PROJECT MANAGER",
+    exp1_desc: `I completed the final project of my university degree as a Software Engineer, entitled “GearWater”. 
+    This project involved the development of a mobile application designed to calculate the amount of water required to irrigate a specific crop, 
+    considering the number of hectares and the geographic location provided by the user. The calculation was performed using complex operations based on user input and application data such as weather conditions, hours of sunlight, water evaporation, and other factors. The project was successfully completed and approved by the university committee. 
+    Developed using <b>Java</b> in <b>Android Studio</b>.`,
+    exp1_date: "APRIL 2020 – DECEMBER 2020 | Organization: Final Degree Project – UDG",
+
+    exp2_title: "Administrative System Development “Candy Spa” | PROJECT MANAGER",
+    exp2_desc: `I designed and implemented a comprehensive management system for a spa, including point-of-sale functionality, inventory control, customer and employee management, report generation, and appointment scheduling. 
+    The system optimized internal processes, improved customer service, and centralized operational data into a single platform. Developed using <b>Visual Basic</b> in <b>Visual Studio</b> and <b>SQL Server</b>.`,
+    exp2_date: "MAY 2016 | Organization: Academic Project",
+
+    exp3_title: "Text and File Encryption Program | PROJECT MANAGER",
+    exp3_desc: ` I developed a desktop application called <b>“ECS: Encrypter Control Security”</b>,
+    capable of encrypting and decrypting text using a custom access key created by
+    the user. The system supports encryption of any type of file, ensuring data security and
+    confidentiality. It includes a user-friendly interface and validation mechanisms
+    to prevent key or format errors. This project was developed using <b>Visual Basic</b>
+    in <b>Visual Studio</b>.`,
+    exp3_date: "JANUARY 2018 | Organization: Academic Project",
+
+    exp4_title: "Online Academic Management System | PROJECT MANAGER",
+    exp4_desc: `  I developed a web platform for educational institutions that allows teachers to
+    assign tasks, upload learning materials, and evaluate submissions, while students
+    can submit assignments, review grades, and access academic resources. The system centralizes academic administration and improves communication
+    between teachers and students, optimizing the monitoring of academic progress.
+    This project was developed using <b>PHP</b> in <b>Atom</b> and <b>MySQL Workbench</b>.`,
+    exp4_date: "MARCH 2018 | Organization: Academic Project",
+    education:"Education",
+    edu1_degree: "Computer Engineering",
+    edu1_school: "University of Guadalajara",
+    edu2_degree: "Technical Degree in Computer and Systems Maintenance",
+    project1_title: "Caesar Cipher Encryptor",
+    project1_desc: `Program developed in Visual Basic that encrypts text using the Caesar cipher.`,
+
+    project2_title: "Music Player",
+    project2_desc: `A music player developed using Java in Android Studio.`,
+
+    project3_title: "Minimum Wage App in Mexico",
+    project3_desc: `An application developed using Java in Android Studio, designed to display the minimum wages that workers should receive according to their job.`
+
+ 
+  }
+};
+
+function setLanguage(lang) {
+  document.querySelectorAll("[data-lang]").forEach(el => {
+    const key = el.dataset.lang;
+    el.innerHTML = texts[lang][key];
+  });
+}
+
+
+const savedLang = localStorage.getItem("lang") || "es";
+setLanguage(savedLang);
